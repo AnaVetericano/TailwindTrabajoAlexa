040abab6d96178ed4fa864d6bdb0ec7d1abf33dd
isabelacodderM <campoisabela07@gmail.com>
2026-08-04 21:24:48 -0500

    create: actualizaradress, formularioadress, listaradress

---
+++ src/app/formularioadress/formularioadress.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularioadress',
+  imports: [],
+  templateUrl: './formularioadress.html',
+  styleUrl: './formularioadress.css',
+})
+export class Formularioadress {
+
+}
---
+++ src/app/listaradress/listaradress.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listaradress',
+  imports: [],
+  templateUrl: './listaradress.html',
+  styleUrl: './listaradress.css',
+})
+export class Listaradress {
+
+}
