cd8cc9ea95be77b864f6f4356a558a3aecf02106
Froshi <kg5892868@gmail.com>
2026-08-04 21:22:36 -0500

    feat: create components: create-customer, list-customer, update-customer

---
+++ src/app/create-customer/create-customer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-customer',
+  imports: [],
+  templateUrl: './create-customer.html',
+  styleUrl: './create-customer.css',
+})
+export class CreateCustomer {
+
+}
---
+++ src/app/list-customer/list-customer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-customer',
+  imports: [],
+  templateUrl: './list-customer.html',
+  styleUrl: './list-customer.css',
+})
+export class ListCustomer {
+
+}
