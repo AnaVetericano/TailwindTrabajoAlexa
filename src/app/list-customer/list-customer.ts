38cbc415235c88a18c23302951585166ef0ba7bf
Froshi <kg5892868@gmail.com>
2026-08-06 14:21:06 -0500

    feat: create componente and logic of a table of list

--- src/app/list-customer/list-customer.ts
+++ src/app/list-customer/list-customer.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+interface InCustomer{
+    customer_id: number,
+    store_id: number,
+    first_name: string,
+    last_name: string,
+    email: string,
+    address_id: number,
+    active: boolean
+}
 
 @Component({
   selector: 'app-list-customer',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './list-customer.html',
   styleUrl: './list-customer.css',
 })
-export class ListCustomer {
 
+export class ListCustomer {
+  NewCustomer: InCustomer[] = []
+  API_CUSTOMER = "https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/customer"
+  constructor(
+    private http: HttpClient, 
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.loadList()
+  }
+
+  loadList(){
+      const headers = {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json'
+      }
+      this.http.get<InCustomer[]>(this.API_CUSTOMER, {headers})
+      .subscribe({
+        next:(response)=>{
+          this.NewCustomer = response
+          this.cdr.detectChanges()
+          console.log(response)
+        }
+      })
+  }
 }
