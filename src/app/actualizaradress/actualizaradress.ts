7c4031d8c546c70f124b2de477dc769911ed2994
isabelacodderM <campoisabela07@gmail.com>
2026-08-07 02:57:57 -0500

    subo cambios en address

--- src/app/actualizaradress/actualizaradress.ts
+++ src/app/actualizaradress/actualizaradress.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-actualizaradress',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './actualizaradress.html',
   styleUrl: './actualizaradress.css',
 })
 export class Actualizaradress {
-
+  id:number=0
+  adress = {
+  address: '',
+  district: '',
+  city_id: 0
+  };
+  constructor( private http:HttpClient,
+  private router:Router,
+  private route:ActivatedRoute
+  ){}
+  ngOnInit(){
+  this.id=this.route.snapshot.params['id']
+  }
+  actualizarAdress(){
+    this.http.put('https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/address?id=eq.'+this.id, 
+    this.adress,
+    {
+      headers: {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json'
+      }
+    }
+    ).subscribe({
+      next: (respuesta) => {
+        alert('Dirección actualizada correctamente');
+        this.router.navigate(['/listaradress']);
+      }
+    })
+  }
 }
--- src/app/listaradress/listaradress.ts
+++ src/app/listaradress/listaradress.ts
@@ -5,14 +5,62 @@ import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 
 interface Adress {
-  
+  address_id: number;
+  address: string;
+  district: string;
+  city_id: number;
 }
 @Component({
   selector: 'app-listaradress',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listaradress.html',
   styleUrl: './listaradress.css',
 })
 export class Listaradress {
+  adress: Adress[] = []
+  constructor(private http: HttpClient, 
+    private router: Router, 
+    private cdr: ChangeDetectorRef) {}
 
+  ngOnInit() {
+    this.traerAdress()
+  }
+  traerAdress() {
+    this.http.get<Adress[]>('https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/address', {
+      headers: {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json',
+      }
+  })
+  .subscribe({
+      next:(response)=>{
+        console.log(response)
+        this.adress=response
+        this.cdr.detectChanges();
+      }
+    })
+  }
+
+  eeliminar(id: number){
+    
+    this.http.delete('https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/address?id=eq. '+ id, 
+    {
+      headers: {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json',
+      }
+    }
+    ).subscribe({
+      next:(respuesta)=>{
+        alert("registro eliminado"+respuesta+ "id"+id)
+        this.traerAdress()
+        this.cdr.detectChanges();
+      }
+    })
+  }
+  llevarACtualizar(id:number){
+    this.router.navigate(['/Actualizaradress',id])
+  }
 }
