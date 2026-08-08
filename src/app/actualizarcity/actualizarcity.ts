437da67f2ace94e22bb8a73d03877e985976a6a1
Froshi <kg5892868@gmail.com>
2026-08-07 21:07:16 -0500

    feat: create a list of citys with features of delete and update city

--- src/app/actualizarcity/actualizarcity.ts
+++ src/app/actualizarcity/actualizarcity.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-actualizarcity',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './actualizarcity.html',
   styleUrl: './actualizarcity.css',
 })
-export class Actualizarcity {}
+export class Actualizarcity {
+  id: number = 0
+  ngOnInit(){
+    this.id = this.route.snapshot.params['id']
+  }
+
+  CityEdit ={
+    city: "",
+    country_id: 0
+  }
+  API_CITY = "https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/city?city_id=eq."
+  constructor(private http: HttpClient, private route: ActivatedRoute){}
+
+  UpdateCity(){
+      const headers = {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json'
+      }
+      
+      this.http.patch(this.API_CITY + this.id, this.CityEdit, {headers})
+      .subscribe({
+        next:(response)=>{
+          alert(`actualización hecha satisfactoriamente` + response)
+        },
+        error:(err)=>{
+          alert('¡¡ERROR DEL SERVIDOR!!')
+          console.log(err);
+          
+        }
+      })
+  }
+}
--- src/app/listarcity/listarcity.ts
+++ src/app/listarcity/listarcity.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+interface InCity{
+    city_id: number,
+    city: number,
+    country_id: string,
+    last_update: string
+}
+
 
 @Component({
   selector: 'app-listarcity',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './listarcity.html',
   styleUrl: './listarcity.css',
 })
-export class Listarcity {}
+export class Listarcity {
+  
+  NewCity: InCity[] = []
+  API_CITY = "https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/city"
+  constructor(
+    private http: HttpClient, 
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.loadList()
+  }
+
+  loadList(){
+      const headers = {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json'
+      }
+      this.http.get<InCity[]>(this.API_CITY, {headers})
+      .subscribe({
+        next:(response)=>{
+          this.NewCity = response
+          this.cdr.detectChanges()
+          console.log(response)
+        }
+      })
+  }
+
+  eliminarCity(id:number){
+      const headers = {
+        apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type': 'application/json'
+      }
+
+      this.http.delete(`${this.API_CITY}?city_id=eq.${id}`,
+        {headers}
+      ).subscribe({
+        next:(response)=>{
+          this.cdr.detectChanges()
+          this.loadList()
+          alert(`Ciudad con id ${id} eliminado correctamente`)
+          console.log(response);
+        }
+      })
+  }
+}
