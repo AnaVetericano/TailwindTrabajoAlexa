a3585670ec3c2780e641b60143059972f81c1a68
AnaVetericano <anyta822@gmail.com>
2026-08-07 19:31:13 -0500

    Feat: formulario city y country

--- src/app/formulariocity/formulariocity.ts
+++ src/app/formulariocity/formulariocity.ts
@@ -11,13 +11,28 @@ import { FormsModule } from '@angular/forms';
 })
 export class Formulariocity {
   nuevoCity={
-    city:''
+    city:'',
+    country_id:0
   }
   constructor(private http:HttpClient){}
-  guardarcity(){
-    this
-  }
+guardarcity(){
+  this.http.post('https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/country',
+    this.nuevoCity,
+    {
+      headers:{
+        apikey:'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        Authorization:'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+        'Content-Type':'application/json'
+      }
+    }).subscribe({
+      next:(respuesta)=>{
+        alert("City guardado correctamente"+respuesta)
+      }
+    }
 
+    )
+}
+}
 
 
 
--- src/app/listarcountry/listarcountry.ts
+++ src/app/listarcountry/listarcountry.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+
+interface Country{
+  country:string
+}
+
 
 @Component({
   selector: 'app-listarcountry',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listarcountry.html',
   styleUrl: './listarcountry.css',
 })
-export class Listarcountry {}
+export class Listarcountry {
+  countrys:Country[]=[]
+}
