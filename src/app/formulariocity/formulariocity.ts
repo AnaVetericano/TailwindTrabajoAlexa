bf857352ae09993786668f500d906ce26d44b273
isabelacodderM <campoisabela07@gmail.com>
2026-08-08 02:41:15 -0500

    commit my changes

--- src/app/formulariocity/formulariocity.ts
+++ src/app/formulariocity/formulariocity.ts
@@ -28,8 +28,5 @@ export class Formulariocity {
 
 
 
-apikey:'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
-        Authorization:'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
-        'Content-Type':'application/json'
 
         }
